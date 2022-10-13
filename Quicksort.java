5f26534fb8d9fb6d20f5e423f6101b09de7d783d
Broti Sunder Bagchi <76203990+Broti2@users.noreply.github.com>
2022-10-13 20:25:23 +0530

    Add files via upload

---
+++ Quicksort.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+class Quicksort {
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    static void quicksort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quicksort(arr, low, pi - 1);
+            quicksort(arr, pi + 1, high);
+        }
+    }
+
+    static void printarry(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 4, 2, 1, 6, 7, 9 };
+        int n = arr.length;
+        quicksort(arr, 0, n - 1);
+        printarry(arr, n);
+    }
+}
